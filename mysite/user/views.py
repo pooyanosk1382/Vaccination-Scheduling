c659922766b3443f667f207eaf4d3e3b11e14827
Pooyan <117625732+pooyanosk1382@users.noreply.github.com>
2024-05-11 22:54:47 +0330

    authorization

--- mysite/user/views.py
+++ mysite/user/views.py
@@ -9,9 +9,11 @@ from django.contrib.auth import get_user_model
 from user.utils import EmailVerificationTokenGenerator
 from django.utils.http import urlsafe_base64_decode
 from django.utils.encoding import force_str
+from django.contrib.auth.decorators import login_required
 
 
 User = get_user_model()
+
 
 def signup(request):
     if request.method == "POST":
@@ -27,6 +29,7 @@ def signup(request):
         "form": SignupForm()
     }
     return render(request, "user/signup.html", context)
+
 
 def login(request):
     if request.method == "POST":
@@ -50,11 +53,14 @@ def login(request):
     return render(request, "user/login.html", context)
 
 
+@login_required()
 def logout(request):
     user_logout(request)
     messages.info(request, "Logged out successfully")
     return HttpResponseRedirect(reverse("user:login"))
 
+
+@login_required()
 def change_password(request):
     if request.method == "POST":
         form = ChangePasswordForm(request.user, request.POST)
@@ -71,12 +77,16 @@ def change_password(request):
     }
     return render(request, "user/change-password.html", context)
 
+
+@login_required()
 def profile_view(request):
     context = {
         "user": request.user
     }
     return render(request, "user/profile-view.html", context)
 
+
+@login_required()
 def profile_update(request):
     if request.method == "POST":
         form = ProfileUpdateForm(request.POST, request.FILES, instance=request.user)
@@ -92,12 +102,16 @@ def profile_update(request):
     }
     return render(request, "user/profile-update.html", context)
 
+
+@login_required()
 def email_verificatin_request(request):
     if not request.user.is_email_verified:
         send_email_verification(request, request.user.id)
         return HttpResponse("Email Verification Link sent to your email address")
     return HttpResponseForbidden("Email already verified")
 
+
+@login_required()
 def email_verifier(request, uidb64, token):
     try:
         uid = force_str(urlsafe_base64_decode(uidb64))
