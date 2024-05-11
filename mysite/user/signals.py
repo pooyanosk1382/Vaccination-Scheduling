2f04f5b793fca1666ee74c37dc21f70bc2329587
Pooyan <117625732+pooyanosk1382@users.noreply.github.com>
2024-05-11 20:48:35 +0330

    signals

---
+++ mysite/user/signals.py
@@ -0,0 +1,20 @@
+import os
+from django.dispatch import receiver
+from django.db import models
+from django.contrib.auth import get_user_model
+
+User = get_user_model()
+
+
+@receiver(models.signals.pre_save, sender=User)
+def auto_delete_file_on_change(sender, instance, **kwargs):
+    try:
+        old_image = User.objects.get(pk=instance.pk).photo
+    except User.DoesNotExist:
+        return False
+
+    new_image = instance.photo
+
+    if bool(old_image) and new_image != old_image:
+        if os.path.isfile(old_image.path):
+            os.remove(old_image.path)
